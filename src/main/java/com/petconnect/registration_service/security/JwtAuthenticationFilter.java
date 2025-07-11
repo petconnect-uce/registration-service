48a8870bf4d41febd3444359f8e8f4e812319547
Diego7118 <dgo7118ldu@gmail.com>
2025-07-11 09:52:09 -0500

    feat:

--- src/main/java/com/petconnect/registration_service/security/JwtAuthenticationFilter.java
+++ src/main/java/com/petconnect/registration_service/security/JwtAuthenticationFilter.java
@@ -30,8 +30,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+            HttpServletResponse response,
+            FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
